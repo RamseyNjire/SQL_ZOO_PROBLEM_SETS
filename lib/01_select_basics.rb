144df3ee1ca6f968c32c893a411854fab3388586
Ramsey Njire <ramseynjire@gmail.com>
2021-04-03 13:37:18 +0300

    finish select basics

--- lib/01_select_basics.rb
+++ lib/01_select_basics.rb
@@ -63,12 +63,24 @@ def scandinavia
   # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE
+      name IN ('Denmark', 'Finland', 'Norway', 'Sweden')
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+     name LIKE 'G%'
   SQL
 end
 
@@ -77,5 +89,12 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+    SELECT
+      name,
+      area/1000 AS thousand_square_kilometers
+    FROM
+      countries
+    WHERE
+      area BETWEEN 200001 AND 249999
   SQL
 end
